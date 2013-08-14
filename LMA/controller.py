e191b2d8a72a1d6ef7b0def270aa3df97d30ade5
Eric <eric.bruning@gmail.com>
2013-08-14 11:32:34 -0500

    Refactor lasso code to use generic lasso payload in stormdrain. Cleanup formatting.

--- LMA/controller.py
+++ LMA/controller.py
@@ -10,10 +10,10 @@ from lmatools.flashsort.autosort.LMAarrayFile import LMAdataFile
 
 from stormdrain.bounds import Bounds, BoundsFilter
 from stormdrain.data import NamedArrayDataset, indexed
-from stormdrain.pipeline import Branchpoint, coroutine, CachedTriggerableSegment, ItemModifier
+from stormdrain.pipeline import Branchpoint, coroutine, ItemModifier
 from stormdrain.support.matplotlib.artistupdaters import scatter_dataset_on_panels
-from stormdrain.support.matplotlib.poly_lasso import LassoFilter
-from stormdrain.pubsub import get_exchange
+from stormdrain.support.matplotlib.poly_lasso import LassoPayloadController
+
 
 
 
@@ -22,9 +22,7 @@ class LMAController(object):
     """
     def __init__(self, *args, **kwargs):
         super(LMAController, self).__init__(*args, **kwargs)
-        self.bounds = Bounds(chi2=(0.0, 1.0),
-                             stations=(6, 99)
-                             )
+        self.bounds = Bounds(chi2=(0.0, 1.0), stations=(6, 99))
         self.datasets = set()
     
     def pipeline_for_dataset(self, d, panels):
@@ -38,7 +36,8 @@ class LMAController(object):
         quality_filter = BoundsFilter(target=brancher, bounds=self.bounds).filter()
         
         transform_mapping = {'z':('alt', (lambda v: (v[0]*1.0e3 - 1.0e3, v[1]*1.0e3 + 1.0e3)) ) }
-        bound_filter = BoundsFilter(target=quality_filter, bounds=panels.bounds, restrict_to=('time'), transform_mapping=transform_mapping)
+        bound_filter = BoundsFilter(target=quality_filter, bounds=panels.bounds, 
+                                    restrict_to=('time'), transform_mapping=transform_mapping)
         filterer = bound_filter.filter()
         d.target = filterer
 
@@ -47,23 +46,20 @@ class LMAController(object):
         scatter_updater = scatter_outlet_broadcaster.broadcast() 
         final_bound_filter = BoundsFilter(target=scatter_updater, bounds=panels.bounds)
         final_filterer = final_bound_filter.filter()
-        cs_transformer = panels.cs.project_points(target=final_filterer, x_coord='x', y_coord='y', z_coord='z', 
-                            lat_coord='lat', lon_coord='lon', alt_coord='alt', distance_scale_factor=1.0e-3)
+        cs_transformer = panels.cs.project_points(
+                            target=final_filterer, 
+                            x_coord='x', y_coord='y', z_coord='z', 
+                            lat_coord='lat', lon_coord='lon', alt_coord='alt',
+                            distance_scale_factor=1.0e-3)
         branch.targets.add(cs_transformer)
 
         # ask for a copy of the array from each selection operation, so that
         # it's saved and ready for any lasso operations
         
-        # @coroutine
-        # def modified_time_printer():
-        #     while True:
-        #         a = (yield)
-        #         for t,c in zip(a['time'], a['charge']):
-        #             print("{0:11.5f}, {1}".format(t,c))
-        
-        # charge_lasso = LassoChargeController(target=ItemModifier(target=modified_time_printer(), item_name='charge').modify())
-
-        charge_lasso = LassoChargeController(target=ItemModifier(target=d.update(field_names=['charge']), item_name='charge').modify())
+        charge_lasso = LassoChargeController(
+                            target=ItemModifier(
+                            target=d.update(field_names=['charge']), 
+                                            item_name='charge').modify())
         scatter_outlet_broadcaster.targets.add(charge_lasso.cache_segment.cache_segment())
 
         # return each broadcaster so that other things can tap into results of transformation of this dataset
@@ -92,57 +88,13 @@ class LMAController(object):
         d = NamedArrayDataset(events[:])
         LMAh5.close()
         return d
+        
 
-class LassoChargeController(object):
-    """ View of this object would be a set of buttons that
-        change the charge state.
+
+
+class LassoChargeController(LassoPayloadController):
+    """ The "charge" attribute is one of {-1, 0, 1} to set 
+        negative, unclassified, or positive charge, or None
+        to do nothing.
     """
-    
-    def __init__(self, *args, **kwargs):
-        """ Register to receive lasso events. 
-            
-            The "charge" attribute is one of {-1, 0, 1} to set 
-            negative, unclassified, or positive charge, or None
-            to do nothing.
-        
-            As a subclass CachedTriggerablePipelineSegment, this class
-            can receive the results of all selection operations, as though
-            it were a plot, so that the current plot state can be subset.
-            The instantiator of this class obtains a target by calling
-            charge_lasso.cache_segment().
-            The target of the cache segment should be an coroutine, perhaps
-            part of another object, that knows what to do with the data.
-                
-            On receiving a new lasso, trigger a resend of the cached data 
-            to the dataset modifier.
-        """
-        self.target = kwargs.pop('target', None)
-        self.charge = None
-        self.lasso_filter = LassoFilter(target=self.add_charge_value(target=self.target))
-        self.lasso_xchg = get_exchange('B4D_panel_lasso_drawn')
-        self.lasso_xchg.attach(self)
-        self.cache_segment = CachedTriggerableSegment(target=self.lasso_filter.filter())
-        
-    @coroutine
-    def add_charge_value(self, target=None):
-        while True:
-            a = (yield)
-            if (self.charge is not None) and (target is not None):
-                target.send((a, self.charge))
-            
-    def send(self, msg):
-        """ B4D_panel_lasso_drawn messages are sent here. 
-        
-            Set the state of the stormdrain.bounds.LassoFilter
-            object to grab the right points.
-        """
-        panels, ax, lasso_line, verts = msg
-        
-        coord_names = panels.ax_specs[ax]
-        self.lasso_filter.coord_names = coord_names
-        self.lasso_filter.verts = verts
-        if self.charge is None:
-            return
-        else:
-            self.cache_segment.resend_last()
-        
+    charge = LassoPayloadController.Payload()        
